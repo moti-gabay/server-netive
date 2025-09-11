a0ea09dae8b7d14c36f3fab5e504ac5debd34c23
moti gabay <motigabay18@gmail.com>
2025-09-11 18:45:08 +0300

    fix cors bug ann add name

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const cookieParser = require("cookie-parser");
 // לאפשר לכולם (כל הדומיינים)
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://192.168.1.92:8081","http://192.168.1.92:5000","http://localhost:8081","http://localhost:5000"], // או כתובת האפליקציה שלך במקום *
+  credentials: true, // אם אתה עובד עם עוגיות
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,7 +26,7 @@ async function startServer() {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
 
-    app.listen(5000, "0.0.0.0",() => console.log("Server running on http://0.0.0.0:5000"));
+    app.listen(5000, "0.0.0.0", () => console.log("Server running on http://0.0.0.0:5000"));
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
   }
